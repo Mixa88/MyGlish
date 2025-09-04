c1fdc410361adf747684c23eacb485d7653c606d
Михаил Тихонов <mishkatix88@gmail.com>
2025-09-04 23:50:03 +0300

    feat: Implement Lessons list view - Create LessonsListView to display lessons with SwiftData @Query. - Add ContentUnavailableView for the empty state. - Implement swipe-to-delete functionality for lessons. - Connect AddLessonView as a sheet via a toolbar button.

---
+++ View/AddLessonView.swift
@@ -0,0 +1,18 @@
+//
+//  AddLessonView.swift
+//  MyGlish
+//
+//  Created by Михайло Тихонов on 04.09.2025.
+//
+
+import SwiftUI
+
+struct AddLessonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddLessonView()
+}
---
+++ View/LessonsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LessonsListView.swift
+//  MyGlish
+//
+//  Created by Михайло Тихонов on 04.09.2025.
+//
+
+import SwiftUI
+
+struct LessonsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LessonsListView()
+}
