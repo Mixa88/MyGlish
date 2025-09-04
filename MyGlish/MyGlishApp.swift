f3a4c638f1839d60aee0dd33e407fce0a0f995f1
Михаил Тихонов <mishkatix88@gmail.com>
2025-09-04 16:17:14 +0300

    Initial Commit

---
+++ MyGlish/MyGlishApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGlishApp.swift
+//  MyGlish
+//
+//  Created by Михайло Тихонов on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyGlishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
