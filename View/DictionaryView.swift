19588cff293f3e5322cb53f3820fe428174731c2
Михаил Тихонов <mishkatix88@gmail.com>
2025-09-07 20:01:15 +0300

    feat: Implement Dictionary view - Create DictionaryView to display all vocabulary words. - Fetch words using SwiftData @Query, sorted by lesson date. - Group words by date and display them in a List with Sections. - Integrate the view into the main TabView.

---
+++ View/DictionaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DictionaryView.swift
+//  MyGlish
+//
+//  Created by Михайло Тихонов on 07.09.2025.
+//
+
+import SwiftUI
+
+struct DictionaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DictionaryView()
+}
